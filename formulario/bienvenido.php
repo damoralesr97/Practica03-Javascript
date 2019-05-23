b545c4e1308011863eb449886d4419842b3adc71
David Morales <damoralesr97@outlook.com>
2019-05-22 20:01:59 -0500

    Galeria realizada, trabajo finalizado

--- formulario/bienvenido.php
+++ formulario/bienvenido.php
@@ -1,3 +1,12 @@
-<?php
-    echo "Hola"
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Bienvenido</title>
+        <link type="text/css" href="style.css" rel="stylesheet">
+    </head>
+    <body>
+        <form id="formulario01">
+            <h1>Bienvenido</h1>
+        </form>
+    </body>
+</html>
